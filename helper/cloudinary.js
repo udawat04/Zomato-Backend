b71f3a3556309a4e3275a40f6c200fea1b04585c
sudarshan udawat <itzudawat@gmail.com>
2025-07-24 00:27:16 +0530

    Create status functionality in restaurant schema and user schema and also create updateStatus api in admin controller so admin can update status

--- helper/cloudinary.js
+++ helper/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: "dqfhn7rw3",
-  api_key: "382695276612379",
-  api_secret: "3XWIpGNiRSe2K2Cs2t9-fUtPPY0",
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 exports.uploadImage = async (files) => {
   const fileArray = Array.isArray(files.images) ? files.images : [files.images]; // Safely extract array of files from `images` key
